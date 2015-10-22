bf054c8d00d035a0594fbd4dc24f168a9b03ec0a
Daniel P. Clark <6ftdan@gmail.com>
2015-10-22 15:59:05 -0400

    Moved commented alternate code out to text file.

--- src/lib.rs
+++ src/lib.rs
@@ -22,20 +22,3 @@ pub fn combinatorial_count(n: i32, k: i32) -> i32 {
   if k > n { panic!("Aaah! First parameter must be the larger number!") };
   permutations(n,k) / factorial(arrayify(k))
 }
-
-//  pub fn slow_permutations(n: i32, k: i32) -> i32 {
-//    if k > n { panic!("Aaah! First parameter must be the larger number!") };
-//    factorial(arrayify(n)) / factorial(arrayify(n-k))
-//  }
-
-//  pub fn slow_combinatorial_count(n: i32, k: i32) -> i32 {
-//    if k > n { panic!("Aaah! First parameter must be the larger number!") };
-//    let m = arrayify(n);
-//    let vals = array_tool::uniques(
-//      m[arrayify(n-k).len()..m.len()].to_vec(),
-//      arrayify(k)
-//    );
-//    factorial(vals[0].to_vec()) / factorial(vals[1].to_vec())
-//  }
-
-
